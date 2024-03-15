5b5df345edc5f0faa2cf8eafbfe97639b1c25a46
Elad Local NNks Course <eladsimantov@gmail.com>
2024-03-15 14:51:32 +0200

    fixed bc added axissymetric pde and added visual

--- Final Project/Membrane_PINNs_Polar.py
+++ Final Project/Membrane_PINNs_Polar.py
@@ -16,7 +16,7 @@ class ffm(nn.Module):
 
 
 class Membrane_PINNs(nn.Module):    
-    def __init__(self, in_dim=3, HL_dim=32, out_dim=1, activation=nn.Tanh(), use_ffm=False):
+    def __init__(self, in_dim=3, HL_dim=16, out_dim=1, activation=nn.Tanh(), use_ffm=False):
         """
         Parameters
         -------------
@@ -40,6 +40,7 @@ class Membrane_PINNs(nn.Module):
         
         # define the network using sequential method
         self.xi = nn.Sequential(*network) 
+
 
     def forward(self, r, theta, t):
         return self.xi(torch.cat((r, theta, t), 1))
@@ -70,18 +71,29 @@ class Membrane_PINNs(nn.Module):
         loss_fun = nn.MSELoss()
 
         # compute the PDE residual loss - only using here solution for initial condition, no external force
-        residual = xi_tt - 1**2 * (xi_ttheta_over_r2 + rxi_rr_over_r)
+        residual = xi_tt - 1**2 * (rxi_rr_over_r) # case axissymetric
+        # residual = xi_tt - 1**2 * (xi_ttheta_over_r2 + rxi_rr_over_r)
         pde_loss = loss_fun(residual, torch.zeros_like(residual))
 
         # compute the BC loss - periodic and with a fixed boundary layer
         xi_reshaped = xi.view(Nr, Ntheta, Nt) # [Nr*Ntheta*Nt, 1] -> [Nr, Ntheta, Nt]
         xi_r_reshaped = xi_r.view(Nr, Ntheta, Nt) # [Nr*Ntheta*Nt, 1] -> [Nr, Ntheta, Nt]
+        xi_theta_reshaped = xi_theta.view(Nr, Ntheta, Nt)
+
+        # The BC loss is a combination of the following:
+        # 1. xi at theta=0 and theta=2pi are equal
+        # 2. xi_theta at theta=0 and theta=2pi are equal
+        # 3. xi are r=R is zero
+        # 4. xi_r at r->0 is zero
         bc_loss = loss_fun(xi_reshaped[Nr-1,:,:], torch.zeros_like(xi_reshaped[Nr-1,:,:])) \
-                + loss_fun(xi_r_reshaped[0,:,:], xi_r_reshaped[Nr-1,:,:]) # derivative at r=R should be symmetric
+                + loss_fun(xi_r_reshaped[0,:,:], torch.zeros_like(xi_r_reshaped[0,:,:])) \
+                + loss_fun(xi_reshaped[:,0,:], xi_reshaped[:,Ntheta-1,:]) \
+                + loss_fun(xi_theta_reshaped[:,0,:], xi_theta_reshaped[:,Ntheta-1,:]) 
         
         # compute the IC loss
         r_reshaped = r.view(Nr, Ntheta, Nt)
         theta_reshaped = theta.view(Nr, Ntheta, Nt)
+        
         #!TODO set proper IC
         xi_initial = torch.cos(2 * np.pi * r_reshaped[:,:,0]) # TBD
         ic_loss = loss_fun(xi_initial, xi_reshaped[:,:,0])
@@ -89,35 +101,45 @@ class Membrane_PINNs(nn.Module):
         return pde_loss, bc_loss, ic_loss
     
 
+    def get_input_tensors(self, r_i, r_f, theta_i, theta_f, t_i, t_f, Nr, Ntheta, Nt):
+        dr = (r_f - r_i) / (Nr-1)
+        dtheta = (theta_f - theta_i) / (Ntheta-1)
+        dt = (t_f - t_i) / (Nt-1)
+        r = torch.zeros(Nr, Ntheta, Nt)
+        theta = torch.zeros(Nr, Ntheta, Nt)
+        t = torch.zeros(Nr, Ntheta, Nt)
+        print(f"Creating input tensors Nr={Nr}, Ntheta={Ntheta}, Nt={Nt}")
+        for i in range(Nr):
+            for j in range(Ntheta):
+                for k in range(Nt):
+                    r[i,j,k] = r_i + dr * i
+                    theta[i,j,k] = theta_i + dtheta * j
+                    t[i,j,k] = t_i + dt * k
+        print("Done!")
+        return r, theta, t
+
+
 
 def main():
-    Nr, Ntheta, Nt = 20, 20, 20
-    rfinal, tfinal = 1, 3
-    r_initial, tinitial = 0.01, 0
-    num_of_epochs = 2000
-    lr = 0.001
-    w_eq, w_bc, w_ic = 1, 20, 20
+    # Set domain bounds and resolution
+    rinitial, rfinal,  = 0.01, 1
+    tinitial, tfinal = 0, 10
     theta_initial, theta_final = 0, 2*np.pi
-    dr = (rfinal - r_initial) / (Nr-1)
-    dtheta = (theta_final - theta_initial) / (Ntheta-1)
-    dt = (tfinal - tinitial) / (Nt-1)
-    print("entered main and defined params")
+    Nr, Ntheta, Nt = 30, 30, 30
+    
+    # Set hyperparams
+    num_of_epochs = 50
+    lr = 0.001
+    w_eq, w_bc, w_ic = 1, 200, 20
+
+    # create PINNs model
+    model = Membrane_PINNs()
 
     # initiallize input parameters as tensors
-    r = torch.zeros(Nr, Ntheta, Nt)
-    theta = torch.zeros(Nr, Ntheta, Nt)
-    t = torch.zeros(Nr, Ntheta, Nt)
-    for i in range(Nr):
-        for j in range(Ntheta):
-            for k in range(Nt):
-                r[i,j,k] = r_initial + dr * i
-                theta[i,j,k] = theta_initial + dtheta * j
-                t[i,j,k] = tinitial + dt * k
+    r, theta, t = model.get_input_tensors(rinitial, rfinal, theta_initial, theta_final, tinitial, tfinal, Nr, Ntheta, Nt)
 
-    model = Membrane_PINNs()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     losses_history = np.zeros((num_of_epochs, 4))
-    print("initiallized... entering loop")
 
     for epoch in range(num_of_epochs):
         eq_loss, BC_loss, IC_loss = model.compute_loss(r.view(-1,1), 
@@ -138,25 +160,53 @@ def main():
         optimizer.step()
         optimizer.zero_grad()
 
-        # skip by 500 epochs before every print
+        # skip by n epochs before every print
         if epoch%1 == 0:
             print(f"epoch: {epoch}, loss: {total_loss}")
         
-        # plot solutions by the end of [10000, 20000] epochs
-        if epoch+1 in [10000, 20000]:
-            xi = model.forward(r.view(-1,1), theta.view(-1,1), t.view(-1,1)) # convert x tensor into a column vector
-            xi_np = xi.detach().numpy() # convert into a np array
-            xi_reshaped = xi_np.reshape(Nr,Ntheta,Nt)
-            #!TODO make a visual meshgrid for membrane vibrations    
-
+    # plot loss history
     plt.plot(losses_history)
     plt.title("Losses History")
     plt.legend(["Total Loss", "PDE Loss", "BC Loss", "IC Loss"])
     plt.show()
+
+    # Create new r theta t vectors for testing and plotting with higher resolution 
+    Nr, Ntheta, Nt = 100, 100, 30
+    r, theta, t = model.get_input_tensors(rinitial, rfinal, theta_initial, theta_final, tinitial, tfinal, Nr, Ntheta, Nt)
+
+    # Test the PINNs
+    xi = model.forward(r.view(-1,1), theta.view(-1,1), t.view(-1,1)) # convert tensors into column vectors
+    xi_np = xi.detach().numpy() # convert xi into a np array
+    xi_reshaped = xi_np.reshape(Nr,Ntheta,Nt) # reshape to fit dimentions
+
+    #!TODO make a visual animated meshgrid for membrane vibrations
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+
+    # Create the mesh in polar coordinates and compute corresponding Z.
+    Radius, Angle = np.meshgrid(np.linspace(rinitial, rfinal, Nr), np.linspace(0, 2*np.pi, Ntheta))
+
+    # Express the mesh in the cartesian system.
+    X, Y = Radius*np.cos(Angle), Radius*np.sin(Angle)
+    Z_0 = xi_reshaped[:,:,0]
+    Z_f = xi_reshaped[:,:,-1]
+
+    # Plot the surface.
+    ax.plot_surface(X, Y, Z_0, cmap=plt.cm.YlGnBu_r)
+    plt.show()
+
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+
+    # Plot the surface.
+    ax.plot_surface(X, Y, Z_f, cmap=plt.cm.YlGnBu_r)
+    ax.set_title("Membrane state at t-tf")
+    plt.show()
+
     return
 
 
 if __name__ == "__main__":
     main()
-    print("finished running all")
+    print("Finished running main without exceptions")
 
